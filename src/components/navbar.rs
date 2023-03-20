b591f803043f7d00c2f61e8f633841d39138f505
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-03-20 09:26:58 +0200

    nothing, just saving

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -1,24 +1,32 @@
-use yew::{function_component, html, Html, Properties};
-use yew_router::prelude::Link;
+use yew::{function_component, html, Callback, Html, Properties};
+use yew_router::prelude::use_navigator;
 
 use crate::app::Route;
+
+#[function_component]
+fn SettingsIcon() -> Html {
+    html! {
+        <svg aria-label="settings icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5" />
+        </svg>
+    }
+}
 
 #[derive(Properties, PartialEq)]
 pub struct Props {}
 
 #[function_component]
 pub fn Navigation(_props: &Props) -> Html {
+    let navigator = use_navigator().unwrap();
+
+    let onclick = Callback::from(move |_| navigator.push(&Route::Settings));
     html! {
         <div id="navbar">
             <div id="top"></div>
             <div id="bottom">
-                <div class="nav_item" id="settings">
-                    <a href="/settings">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 13.5V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 010 3m0-3a1.5 1.5 0 000 3m0 9.75V10.5" />
-                        </svg>
-                    </a>
-                </div>
+                <button {onclick} class="nav_item" id="settings">
+                    <SettingsIcon />
+                </button>
             </div>
         </div>
     }
