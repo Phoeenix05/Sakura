87928edd563c02c994958ca58bd3cd4aab248704
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-07-29 11:53:18 +0300

    chore(config): updated dependency path

--- src/routes/manga/[id].tsx
+++ src/routes/manga/[id].tsx
@@ -2,11 +2,11 @@ import { createResource } from "solid-js";
 import { useParams } from "solid-start";
 import { ID } from "~/util/props";
 
-const fetch_chapter_data = async (id: string): Promise<any> => { }
+const fetch_manga_data = async (id: string): Promise<any> => { }
 
 export default function MangaId() {
     const { id } = useParams<ID>();
-    const [res, { mutate, refetch }] = createResource(id, fetch_chapter_data, {});
+    const [res, { mutate, refetch }] = createResource(id, fetch_manga_data, {});
 
     return (
         <main></main>
