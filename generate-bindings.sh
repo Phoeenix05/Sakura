fe2a602c177933ebcf08900fcbb614a532a50a34
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-07-29 13:12:36 +0300

    feat: added script for generating api bindings

---
+++ generate-bindings.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+ROOT_DIR=$(pwd)
+API_DIR=$ROOT_DIR/mangadex-api-tsrs
+BINDINGS_DIR=$ROOT_DIR/src/util/bindings
+
+mkdir -p $BINDINGS_DIR
+
+generate_bindings() {
+    cd ${1:?"Path must be specified"}
+    cargo test
+    mv bindings/*.ts $BINDINGS_DIR/
+}
+
+generate_bindings $API_DIR/mangadex-api-schema
+generate_bindings $API_DIR/mangadex-api-types
