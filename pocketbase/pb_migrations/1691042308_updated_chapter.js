367a61d59bbb12361ba36f7b939194336eb7f767
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-08-03 15:08:01 +0300

    feat(db): added pocketbase as db / cache

---
+++ pocketbase/pb_migrations/1691042308_updated_chapter.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("gib2oh1ms3uymes")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "usdwwij6",
+    "name": "chapter_id",
+    "type": "text",
+    "required": true,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": "^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$"
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("gib2oh1ms3uymes")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "usdwwij6",
+    "name": "chapter_id",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": "^[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}$"
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
