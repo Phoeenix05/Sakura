b41191af1322c4f2b847142da0b759af59783012
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-02-25 23:04:37 +0200

    chore: config changes and removed template stuff

--- src-tauri/src/mac/window.rs
+++ src-tauri/src/mac/window.rs
@@ -372,7 +372,7 @@ pub fn setup_mac_window(app: &mut App) {
     app.get_window("main").unwrap().set_transparent_titlebar();
 
     let window_handle = app.get_window("main").unwrap();
-    update_window_theme(&window_handle, HexColor::WHITE);
+    update_window_theme(&window_handle, HexColor::BLACK);
 
     // Control window theme based on app update_window
     app.listen_global("hopp-bg-changed", move |ev| {
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav data-tauri-drag-region></nav>
+</template>
+
+<style scoped lang="less">
+nav {
+  box-sizing: border-box;
+  position: fixed;
+  overflow: hidden;
+  z-index: 99;
+
+  width: 100vi;
+  height: 50px;
+  top: 0;
+
+  border-bottom: 1px red solid;
+}
+</style>
