74c68170a43dbc91eabbe4e5f5a497b9625bb3a4
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-02-25 14:25:15 +0200

    feat: init tauri

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
