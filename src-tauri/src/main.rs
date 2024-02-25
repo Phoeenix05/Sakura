cdbcae046c4cbcb5ed04ea58378231cfe65b9bdd
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-02-25 14:47:34 +0200

    feat: stole code from hoppscotch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,30 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate cocoa;
+
+#[cfg(target_os = "macos")]
+#[macro_use]
+extern crate objc;
+
+#[cfg(target_os = "macos")]
+mod mac;
+
+#[cfg(target_os = "windows")]
+mod win;
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(target_os = "macos")]
+            crate::mac::window::setup_mac_window(app);
+            #[cfg(target_os = "windows")]
+            crate::win::window::setup_win_window(app);
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
